ac70f54341c40a5ce41c4945b773a398da0afeb8
JtPerez-Acle <jtperez.acle@gmail.com>
2023-08-28 16:55:45 -0400

    Final -V; Rust; Fahr to Cel

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Enter the temperatue in Fahrenheit:");
+
+    let mut fahrenheit_temp = String::new();
+
+    io::stdin()
+        .read_line(&mut fahrenheit_temp)
+        .expect("Encountered an error!");
+
+    let fahrenheit: f64 = fahrenheit_temp.trim().parse().expect("Please enter a valid number.");
+
+    let celsius = (fahrenheit - 32.0) * 5.0/9.0;
+
+    println!("{}F is equal to {}C", fahrenheit, celsius);     
+    
 }
